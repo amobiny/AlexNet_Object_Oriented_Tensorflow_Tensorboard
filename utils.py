79cdd8412d8e3e13ce638e213792d30827c0ee69
Aryan Mobiny <amobiny@uh.edu>
2017-07-12 11:32:31 -0500

    Adding Files

---
+++ utils.py
@@ -0,0 +1,56 @@
+"""
+Copyright 2017-2022 Department of Electrical and Computer Engineering
+University of Houston, TX/USA
+**********************************************************************************
+Author:   Aryan Mobiny
+Date:     6/1/2017
+Comments: Python utility functions
+**********************************************************************************
+"""
+
+import numpy as np
+import random
+import scipy
+
+def randomize(x, y):
+    """ Randomizes the order of data samples and their corresponding labels"""
+    permutation = np.random.permutation(y.shape[0])
+    shuffled_x = x[permutation, :, :, :]
+    shuffled_y = y[permutation]
+    return shuffled_x, shuffled_y
+
+
+def reformat(x, y, img_size, num_ch, num_class):
+    """ Reformats the data to the format acceptable for the conv layers"""
+    dataset = x.reshape(
+        (-1, img_size, img_size, num_ch)).astype(np.float32)
+    labels = (np.arange(num_class) == y[:, None]).astype(np.float32)
+    return dataset, labels
+
+def get_next_batch(x, y, start, end):
+    x_batch = x[start:end]
+    y_batch = y[start:end]
+    return x_batch, y_batch
+
+
+def random_rotation_2d(batch, max_angle):
+    """ Randomly rotate an image by a random angle (-max_angle, max_angle).
+
+    Arguments:
+    max_angle: `float`. The maximum rotation angle.
+
+    Returns:
+    batch of rotated 2D images
+    """
+    size = batch.shape
+    batch = np.squeeze(batch)
+    batch_rot = np.zeros(batch.shape)
+    for i in range(batch.shape[0]):
+        if bool(random.getrandbits(1)):
+            image = np.squeeze(batch[i])
+            angle = random.uniform(-max_angle, max_angle)
+            batch_rot[i] = scipy.ndimage.interpolation.rotate(image, angle, mode='nearest', reshape=False)
+        else:
+            batch_rot[i] = batch[i]
+    return batch_rot.reshape(size)
+
